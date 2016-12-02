90447c5b74704ee12574345cc05372f9146b43ee
Andreas Altheimer <andreas@altheimer.at>
2016-12-02 14:14:58 +0100

    js fix

--- other-projects/ytplayer.js
+++ other-projects/ytplayer.js
@@ -1,16 +1,19 @@
 var player, seconds = 0;
 function onYouTubeIframeAPIReady() {
-    console.log("player");
+    
     player = new YT.Player('player', {
         events: {
           'onReady': onPlayerReady
         }
+		console.log("player");
       });
 	playerTwo = new YT.Player('playerTwo', {
         events: {
           'onReady': onPlayerReady
         }
+		console.log("player2");
 	});
+	
 }
 
 function onPlayerReady(event) {
